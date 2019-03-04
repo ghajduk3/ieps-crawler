7d80164eadda7c6e7e5845d66645e826002118aa
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-03-04 23:27:39 +0100

    database nearly complete

--- src/main/scala/com/ieps/crawler/db/CrawlerDIO.scala
+++ src/main/scala/com/ieps/crawler/db/CrawlerDIO.scala
@@ -1,34 +1,93 @@
 package com.ieps.crawler.db
 
+import com.ieps.crawler.db
+import com.ieps.crawler.db.CrawlerDIO.Query.{writePage, writeSite}
 import com.ieps.crawler.db.Tables._
 import slick.dbio.DBIO
 import slick.jdbc.PostgresProfile.api._
 
-class CrawlerDIO {
+import scala.concurrent.ExecutionContext
+
+object CrawlerDIO {
+  implicit val ec: ExecutionContext = ExecutionContext.global
 
   def findSiteById(id: Int): DBIO[Option[SiteRow]] = Query.siteById(id).result.headOption
   def findPageById(id: Int): DBIO[Option[PageRow]] = Query.pageById(id).result.headOption
-  def findSiteWithPages: DBIO[Seq[(SiteRow, PageRow)]] = Query.siteWithPages.result
+  def findPageByIds(ids: Seq[Int]): DBIO[Seq[PageRow]] = Page.filter(_.id inSet ids).result
+  def findPageByLinkTarget(links: Seq[LinkRow]): DBIO[Seq[PageRow]] = Page.filter(_.id inSet links.map(_.toPage)).result
+  def findSiteWithPages: DBIO[Seq[(SiteRow, PageRow)]] = Query.getSiteWithPages.result
+  def findImagesByPageId(id: Int): DBIO[Seq[ImageRow]] = Image.filter(_.pageId === id).result
+  def findPageDatumByPageId(id: Int): DBIO[Seq[PageDataRow]] = PageData.filter(_.pageId === id).result
+  def findLinksBySourceId(id: Int): DBIO[Seq[LinkRow]] = Query.linksByFromId(id).result
 
+  def getPageLinksById(id: Int): DBIO[(Option[PageRow], Seq[PageRow])] = for {
+    sourcePage <- findPageById(id)
+    links <- findLinksBySourceId(id)
+    targetPages <- findPageByLinkTarget(links)
+  } yield (sourcePage, targetPages)
+
+  def getPageContents(id: Int): DBIO[(Seq[ImageRow], Seq[PageDataRow])] = for {
+    images <- findImagesByPageId(id)
+    pageDatum <- findPageDatumByPageId(id)
+  } yield (images, pageDatum)
+
+  // get static tables
+  def getPageTypes: DBIO[Seq[PageTypeRow]] = Query.pageTypes
+  def getDataTypes: DBIO[Seq[DataTypeRow]] = Query.dataTypes
+
+  // insert statements
   def insertSite(site: SiteRow): DBIO[SiteRow] = Query.writeSite += site
   def insertPage(page: PageRow): DBIO[PageRow] = Query.writePage += page
+  def insertPages(pages: Seq[PageRow]): DBIO[Seq[PageRow]] = Query.writePage ++= pages
+  def insertLink(link: LinkRow): DBIO[LinkRow] = Query.writeLink += link
+  def linkPages(fromPage: PageRow, toPage: PageRow): DBIO[LinkRow] = insertLink(LinkRow(fromPage.id, toPage.id))
+  def insertImage(image: ImageRow): DBIO[ImageRow] = Query.writeImage += image
+  def insertImages(images: Seq[ImageRow]): DBIO[Seq[ImageRow]] = Query.writeImage ++= images
+  def insertPageData(pageData: PageDataRow): DBIO[PageDataRow] = Query.writePageData += pageData
+  def insertPageData(pageData: Seq[PageDataRow]): DBIO[Seq[PageDataRow]] = Query.writePageData ++= pageData
 
-//  def sitesWithPages() = Query.siteWithPages.result
+  // insert site with pages
+  def insertSiteWithPage(site: SiteRow, page: PageRow): DBIO[(SiteRow, PageRow)] = for {
+    site <- insertSite(site)
+    page <- insertPage(page.copy(siteId = Some(site.id)))
+  } yield (site, page)
+
+  def insertSiteWithPages(site: SiteRow, pages: Seq[PageRow]): DBIO[(SiteRow, Seq[PageRow])] = for {
+    site <- insertSite(site)
+    page <- insertPages(pages.map(page => page.copy(siteId = Some(site.id))))
+  } yield (site, page)
+
+  def insertPageWithContent(
+   page: PageRow,
+   images: Seq[ImageRow],
+   pageDatum: Seq[PageDataRow]
+  ): DBIO[(PageRow, Seq[ImageRow], Seq[PageDataRow])] =
+    for {
+      insertedPage <- insertPage(page)
+      insertedImages <- insertImages(images.map(image => image.copy(pageId = Option(insertedPage.id))))
+      insertedPageData <- insertPageData(pageDatum.map(pageData => pageData.copy(pageId = Option(insertedPage.id))))
+    } yield(insertedPage, insertedImages, insertedPageData)
 
   object Query {
     import Tables._
 
     val writeSite = Site returning Site.map(_.id) into((site, id) => site.copy(id))
     val writePage = Page returning Page.map(_.id) into((page, id) => page.copy(id))
+    val writeLink = Link returning Link
+    val writePageData = PageData returning PageData.map(_.id) into((pageData, id) => pageData.copy(id))
+    val writeImage = Image returning Image.map(_.id) into((image, id) => image.copy(id))
 
     val siteById = Site.findBy(_.id)
     val pageById = Page.findBy(_.id)
+    val linksByFromId = Link.findBy(_.fromPage)
+    val pageTypes = PageType.result
+    val dataTypes = DataType.result
 
-    val siteWithPages = for {
+    // get site with pages
+    val getSiteWithPages = for {
       site <- Site
       page <- Page if page.siteId === site.id
     } yield (site, page)
-
   }
 
 }
---
+++ src/main/scala/com/ieps/crawler/db/DBService.scala
@@ -0,0 +1,88 @@
+package com.ieps.crawler.db
+
+import com.ieps.crawler.db
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class DBService(dbConfig: String) {
+  import Tables._
+
+  val db = Database.forConfig(dbConfig)
+  implicit val timeout: FiniteDuration = new FiniteDuration(1, MINUTES)
+
+  // Site
+  def getSiteByIdFuture(id: Int): Future[Option[SiteRow]] =
+    db.run(CrawlerDIO.findSiteById(id))
+
+  def getSiteById(id: Int): Option[SiteRow] =
+    Await.result(getSiteByIdFuture(id), timeout)
+
+  def insertSiteFuture(site: SiteRow): Future[SiteRow] =
+    db.run(CrawlerDIO.insertSite(site))
+
+  def insertSite(site: SiteRow): SiteRow =
+    Await.result(insertSiteFuture(site), timeout)
+
+  // Page
+  def getPageByIdFuture(id: Int): Future[Option[PageRow]] =
+    db.run(CrawlerDIO.findPageById(id))
+
+  def getPageById(id: Int): Option[PageRow] =
+    Await.result(getPageByIdFuture(id), timeout)
+
+  def insertPageFuture(page: PageRow): Future[PageRow] =
+    db.run(CrawlerDIO.insertPage(page))
+
+  def insertPage(page: PageRow): PageRow =
+    Await.result(insertPageFuture(page), timeout)
+
+  // bulk insert
+  def insertSiteWithPageFuture(site: SiteRow, page: PageRow): Future[(SiteRow, PageRow)] =
+    db.run(CrawlerDIO.insertSiteWithPage(site, page))
+
+  def insertSiteWithPage(site: SiteRow, page: PageRow): (SiteRow, PageRow) =
+    Await.result(insertSiteWithPageFuture(site, page), timeout)
+
+  def insertSiteWithPagesFuture(site: SiteRow, page: Seq[PageRow]): Future[(SiteRow, Seq[PageRow])] =
+    db.run(CrawlerDIO.insertSiteWithPages(site, page))
+
+  def insertSiteWithPages(site: SiteRow, page: Seq[PageRow]): (SiteRow, Seq[PageRow]) =
+    Await.result(insertSiteWithPagesFuture(site, page), timeout)
+
+  def insertPageWithContentFuture(page: PageRow, images: Seq[ImageRow], pageDatum: Seq[PageDataRow]): Future[(PageRow, Seq[ImageRow], Seq[PageDataRow])] =
+    db.run(CrawlerDIO.insertPageWithContent(page, images, pageDatum))
+
+  def insertPageWithContent(page: PageRow, images: Seq[ImageRow], pageDatum: Seq[PageDataRow]): (PageRow, Seq[ImageRow], Seq[PageDataRow]) =
+    Await.result(insertPageWithContentFuture(page, images, pageDatum), timeout)
+
+  def linkPagesFuture(fromPage: PageRow, toPage: PageRow): Future[LinkRow] =
+    db.run(CrawlerDIO.linkPages(fromPage, toPage))
+
+  def linkPages(fromPage: PageRow, toPage: PageRow): LinkRow =
+    Await.result(linkPagesFuture(fromPage, toPage), timeout)
+
+  def getPageLinksFuture(id: Int): Future[(Option[PageRow], Seq[PageRow])] =
+    db.run(CrawlerDIO.getPageLinksById(id))
+
+  def findPagesByLinkTarget(links: Seq[LinkRow]): Future[Seq[PageRow]] =
+    db.run(CrawlerDIO.findPageByLinkTarget(links))
+
+  def getPageLinks(id: Int): Seq[PageRow] =
+    Await.result(getPageLinksFuture(id), timeout)._2
+
+  def getPageLinks(page: PageRow): Seq[PageRow] =
+    getPageLinks(page.id)
+
+  def getPageContentFuture(id: Int): Future[(Seq[ImageRow], Seq[PageDataRow])] =
+    db.run(CrawlerDIO.getPageContents(id))
+
+  def getPageContent(id: Int): (Seq[ImageRow], Seq[PageDataRow]) =
+    Await.result(getPageContentFuture(id), timeout)
+
+  def getPageContent(page: PageRow): (Seq[ImageRow], Seq[PageDataRow]) =
+    Await.result(getPageContentFuture(page.id), timeout)
+
+  def closeDb(): Unit = db.close()
+}
